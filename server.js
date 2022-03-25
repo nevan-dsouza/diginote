4f014716142a2bf60d073d7264d2986e28252d11
Nevan <nev.kd2832@gmail.com>
2022-03-25 18:18:11 -0400

    added basic setup code to use express

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
+// Dependancies required for the application:
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
+// Port decided by the domain, otherwise use 3001
+const port = process.env.PORT || 3001;
+
+// Set up express by assigning it to app
+const app = express();
+
+// Middleware to parse through JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
